323f52aae971a4eeb8343dfff406bb49639e39ca
BryanMums <bryan.muhmenthaler@gmail.com>
2017-05-04 11:14:22 +0200

    Enlever le dossier sandbox du repo

--- sandbox/sketch_app/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/bryan/Documents/School/TB/Ruschli/sandbox/sketch_app/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
